100ed086ef1c19fa68398b72fd6bb7f812be6e20
Kanthon <kanthon1209@outlook.com>
2025-10-23 21:40:24 +0800

    JSON内容以JSON类型存入 milvus 数据库

--- src/milvus/collection_create.py
+++ src/milvus/collection_create.py
@@ -12,15 +12,15 @@ fields = [
     FieldSchema(name="id", dtype=DataType.INT64, is_primary=True, auto_id=True),  # 自动ID, 所以插入的时候不要传入 ids
     FieldSchema(name="source", dtype=DataType.VARCHAR, max_length=1000),
     FieldSchema(name="sentence", dtype=DataType.VARCHAR, max_length=10000),
-    FieldSchema(name="coarse_types", dtype=DataType.VARCHAR, max_length=2000),   # 可以存 JSON 字符串化后的数组
-    FieldSchema(name="entities", dtype=DataType.VARCHAR, max_length=5000),       # 同样存 JSON 字符串化后的对象数组
+    FieldSchema(name="coarse_types", dtype=DataType.JSON, max_length=2000),   # 可以存 JSON 字符串化后的数组
+    FieldSchema(name="entities", dtype=DataType.JSON, max_length=5000),       # 同样存 JSON 字符串化后的对象数组
     FieldSchema(name="embedding", dtype=DataType.FLOAT_VECTOR, dim=4096)         # Qwen3 embedding 输出维度
 ]
 
 schema = CollectionSchema(fields, description="NER training data (new format)")
 
 # 3️⃣ Collection 名称
-collection_name = "rag_qwen"
+collection_name = "rag_qwen_json"
 # 4️⃣ 检查 Collection 是否存在
 available_collections = utility.list_collections()
 
