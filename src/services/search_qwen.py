1db3c8d4ceefa855bfa879079714dc05f0d616b2
Kanthon <kanthon1209@outlook.com>
2025-10-26 09:34:41 +0800

    Embedding 服务 和 Rerrank 服务集成到一个模块中

--- src/services/search_qwen.py
+++ src/services/search_qwen.py
@@ -7,19 +7,19 @@ import logging
 import time
 import json
 
-from src.qwen.embedding import EmbeddingClient
+from src.qwen.remote_services import Client
 from src.utils.misc import timer
 from src.services.postprocess.rerank import compute_entity_name_match_score, compute_entity_overlap_score
 
-collection_name = "rag_qwen"            # 没有用 JSON 字段的 数据库 (过滤过的)
+collection_name = "rag_qwen"            # 没用 JSON 字段的 数据库 (过滤过的)
 collection_v2_name = "rag_qwen_json"    # 用了 JSON 字段的数据库 (过滤过的)
 collection_v3_name = "rag_qwen_origin"  # 用了 JSON 字段的数据库, 没有经过过滤, 用的原始的训练样本
-collection_v4_name = "rag_qwen_v4"  # 用了 JSON 字段的数据库, 没有经过过滤, 用的原始的训练样本
+collection_v4_name = "rag_qwen_v4"      # 用了 JSON 字段的数据库, 没有经过过滤, 用的原始的训练样本
 global_counter = 0
 
 app = FastAPI()
 logger = logging.getLogger("app")
-client = EmbeddingClient()
+client = Client()
 
 # ===== 1️⃣ 初始化 Milvus 连接 =====
 connections.connect("default", host="127.0.0.1", port="19530")
@@ -34,177 +34,12 @@ collection_v4.load() # 加载集合到内存
 
 @timer(logger=logger)
 def create_embedding(query_text: str) -> list[float]:
-    
     resp = client.embed(query_text)
     query_vector = resp['embeddings'][0]
     return query_vector
 
-
-
-
-# ===== 3️⃣ 定义 POST 接口 =====
-@app.post("/v1/search")
-async def search_items(request: Request):
-    start_time = time.time()
-    global global_counter
-    global_counter += 1
-
-    body = await request.json()
-
-    # 提取请求字段
-    query_text = body.get("sentence", None)
-    limit = body.get("limit", 50)
-    test_coarse_types: list[str] = body.get("coarse_types", [])
-    # TODO: 以测试样本 coarse_types 字段为依据, 每一个 type 从训练集中找一个符合的训练样本供给参考
-
-
-    logger.info(f"{global_counter:<5} received POST /search from {request.client.host}:{request.client.port}")
-    output_fields = body.get("output_fields", ["id", "source", "coarse_types", "sentence", "entities"])
-    #TODO: 参数校验
-
-    if not query_text:
-        return JSONResponse({"error": "missing 'sentence' field"}, status_code=400)
-
-    # ===== 生成 embedding =====
-    try:
-        query_vector = create_embedding(query_text)
-    except Exception as e:
-        return JSONResponse({"error": f"embedding generation failed: {e}"}, status_code=500)
-    
-    # ===== 执行 Milvus 向量检索 =====
-    search_params = {
-        "metric_type": "IP",  # 必须和 index 一致, 改成 Inner Product 内积
-        "params": {"ef": 128} # NOTE: ef: 查询时的搜索范围，越大精度越高、但速度略慢
-    }
-    
-    # 从服务器获取 embedding 的时候, 服务器上做了 L2 归一化, 这里 内积（Inner Product, IP） 和 余弦相似度（Cosine similarity）是等价的
-    try:
-        results = collection.search(
-            data=[query_vector],
-            anns_field="embedding",
-            param=search_params,
-            limit=limit,
-            output_fields=output_fields
-        )
-    except Exception as e:
-        return JSONResponse({"error": f"milvus search failed: {e}"}, status_code=500)
-
-    # ===== 整理返回结果 =====
-    hits_list = []
-    for hits in results:
-        for hit in hits:
-            d = {}
-            entity = hit.entity
-            d["score"] = hit.distance
-            for field in output_fields:
-                d[field] = entity.get(field)
-            # if "coarse_types" in d: # NOTE: 将 coarse_types 字符串转换为列表
-            #     d["coarse_types"] = d["coarse_types"].split(",")
-            if "coarse_types" in d:
-                d["coarse_types"] = json.loads(d["coarse_types"])
-            if "entities" in d:
-                d["entities"] = json.loads(d["entities"])
-                if test_coarse_types:
-                    overlap_score = compute_entity_overlap_score(test_coarse_types, d["entities"])
-                    d['coarse_types_overlap_score'] = overlap_score
-                else:
-                    d['coarse_types_overlap_score'] = 0
-                entity_name_match_score = compute_entity_name_match_score(query_text, d["entities"])
-                d['entity_name_match_score'] = entity_name_match_score
-                d['mix_score'] = (d['entity_name_match_score'] + 0.1) * (d['coarse_types_overlap_score'] + 0.1)
-            hits_list.append(d)
-
-    # 根据度量类型来
-    hits_list.sort(key=lambda x: (-x['mix_score'])) # 按照 overlap_score 先排序, 后 score 排序
-    end_time = time.time()
-    return JSONResponse(
-            {
-                "results": hits_list, 
-                "help": 
-                """1. Choosable return fields: ...\n2. default limit value is 10\n3. use coarse_types filed to specify the range of the recognition\n4. 训练样本中, 返回的三元组中的粗粒度标签可能不在要求的 coarse_types 中, 如果需要过滤这一部分的三元组, 避免他们出现在 output 中, 指定 filter_coarse_type 字段为 true, 注意, 不要使用引号包裹 true; filter_coarse_type 不指定值的时候, 等同于设置为 false\n5. """,
-                "info": "version 1.0",
-                "cost_time": f"{end_time - start_time:.4f}s"
-            }, status_code=200)
-
-
-@app.post("/v2/search")
-async def search_items_2(request: Request):
-    start_time = time.time()
-    global global_counter
-    global_counter += 1
-
-    body = await request.json()
-
-    # 提取请求字段
-    query_text = body.get("sentence", None)
-    limit = body.get("limit", 50)
-    test_coarse_types: list[str] = body.get("coarse_types", [])
-    # TODO: 以测试样本 coarse_types 字段为依据, 每一个 type 从训练集中找一个符合的训练样本供给参考
-
-
-    logger.info(f"{global_counter:<5} received POST /search from {request.client.host}:{request.client.port}")
-    output_fields = body.get("output_fields", ["id", "source", "coarse_types", "sentence", "entities"])
-    #TODO: 参数校验
-
-    if not query_text:
-        return JSONResponse({"error": "missing 'sentence' field"}, status_code=400)
-
-    # ===== 生成 embedding =====
-    try:
-        query_vector = create_embedding(query_text)
-    except Exception as e:
-        return JSONResponse({"error": f"embedding generation failed: {e}"}, status_code=500)
-    
-    # ===== 执行 Milvus 向量检索 =====
-    search_params = {
-        "metric_type": "IP",  # 必须和 index 一致, 改成 Inner Product 内积
-        "params": {"ef": 128} # NOTE: ef: 查询时的搜索范围，越大精度越高、但速度略慢
-    }
-    
-    # 从服务器获取 embedding 的时候, 服务器上做了 L2 归一化, 这里 内积（Inner Product, IP） 和 余弦相似度（Cosine similarity）是等价的
-    try:
-        results = collection_v2.search(
-            data=[query_vector],
-            anns_field="embedding",
-            param=search_params,
-            limit=limit,
-            output_fields=output_fields
-        )
-    except Exception as e:
-        return JSONResponse({"error": f"milvus search failed: {e}"}, status_code=500)
-
-    # ===== 整理返回结果 =====
-    hits_list = []
-    for hits in results:
-        for hit in hits:
-            d = {}
-            entity = hit.entity
-            d["score"] = hit.distance
-            for field in output_fields:
-                d[field] = entity.get(field)
-            # NOTE:
-            if test_coarse_types:
-                overlap_score = compute_entity_overlap_score(test_coarse_types, d["entities"])
-                d['coarse_types_overlap_score'] = overlap_score
-            else:
-                d['coarse_types_overlap_score'] = 0
-            entity_name_match_score = compute_entity_name_match_score(query_text, d["entities"])
-            d['entity_name_match_score'] = entity_name_match_score
-            d['mix_score'] = (d['entity_name_match_score'] + 0.1) * (d['coarse_types_overlap_score'] + 0.1)
-            # NOTE:
-            hits_list.append(d)
-
-    # 根据度量类型来
-    hits_list.sort(key=lambda x: (-x['mix_score'])) # 按照 overlap_score 先排序, 后 score 排序
-    end_time = time.time()
-    return JSONResponse(
-            {
-                "results": hits_list, 
-                "info": "version 2.0",
-                "cost_time": f"{end_time - start_time:.4f}s"
-            }, status_code=200)
-
-
+def rerank(query: str, docs: list[str], model_name: str = "Qwen3-Reranker-4B") -> list[float]:
+    return client.rerank(query=query, docs=docs, model_name=model_name)
 
 @app.post("/v3/search")
 async def search_v3(request: Request):
@@ -217,13 +52,21 @@ async def search_v3(request: Request):
     # 提取请求字段
     query_text = body.get("sentence", None)
     limit = body.get("limit", 50)
-    methods = ['default', "mix"]
+    methods = ["default", "coarse_only", "type_first", "name_first", "mix", "rerank", "rerank-0.6b", "rerank-4b", "rerank-8b"]
     method = body.get("method", "default")
-    if method not in methods:
-        return JSONResponse({"error": f"wrong method, method should be in {methods}"}, status_code=400)
+    test_coarse_types: list[str] = body.get("coarse_type", [])
 
-    test_coarse_types: list[str] = body.get("coarse_types", [])
-    # TODO: 以测试样本 coarse_types 字段为依据, 每一个 type 从训练集中找一个符合的训练样本供给参考
+
+    # 判断使用的方法
+    if method not in methods: # 选择的方法不在 定义好的 methods 中的话
+        return JSONResponse({"error": f"wrong method, method should be in {methods}"}, status_code=400)
+    
+
+    # TODO: 以测试样本 coarse_types 字段为依据, 从训练集中找出若干样本, 这些样本的 entity.coarse_type 可以覆盖测试样本的 coarse_type
+    if method == 'coarse_only':
+        if test_coarse_types is None or len(test_coarse_types) == 0:
+            return JSONResponse({"error": "no test sample coarse_type"})
+        return select_train_samples_by_coarse_type(test_coarse_types)
 
     logger.info(f"{global_counter:<5} received POST /search from {request.client.host}:{request.client.port}")
     output_fields = body.get("output_fields", ["id", "source", "coarse_types", "sentence", "entities"])
@@ -267,23 +110,40 @@ async def search_v3(request: Request):
             # NOTE:
             if test_coarse_types:
                 overlap_score = compute_entity_overlap_score(test_coarse_types, d["entities"])
-                d['coarse_types_overlap_score'] = overlap_score
+                d['type_overlap_score'] = overlap_score
             else:
-                d['coarse_types_overlap_score'] = 0
+                d['type_overlap_score'] = 0
             entity_name_match_score = compute_entity_name_match_score(query_text, d["entities"])
-            d['entity_name_match_score'] = entity_name_match_score
-            d['mix_score'] = (d['entity_name_match_score'] + 0.1) * (d['coarse_types_overlap_score'] + 0.1)
+            d['name_overlap_score'] = entity_name_match_score
+            d['mix_score'] = (d['name_overlap_score'] + 0.1) * (d['type_overlap_score'] + 0.1)
             # NOTE:
             hits_list.append(d)
 
     # 根据度量类型来
-    if method == 'mix':
+    if method == 'mix': #如果是 mix 重排策略, 将 hits_list 重新排序
         hits_list.sort(key=lambda x: (-x['mix_score'])) # 按照 overlap_score 先排序, 后 score 排序
+    if method == 'type_first':
+        hits_list.sort(key = lambda x: (-x['type_overlap_score']))
+    if method == 'name_first':
+        hits_list.sort(key = lambda x: (-x['name_overlap_score']))
+    if method in ["rerank", "rerank-0.6b", "rerank-4b", "rerank-8b"]:
+        method_model_map = {
+            "rerank": "Qwen3-Reranker-4B", # 默认用 4B
+            "rerank-0.6b": "Qwen3-Reranker-0.6B",
+            "rerank-4b": "Qwen3-Reranker-4B",
+            "rerank-8b": "Qwen3-Reranker-8B",
+        }
+        model_name = method_model_map[method]
+        reranked_scores = rerank(query=query_text, docs=[item['sentence'] for item in hits_list], model_name=model_name)['scores']
+        for idx, item in enumerate(hits_list):
+            item['rerank_score'] = reranked_scores[idx]
+        hits_list.sort(key = lambda x: -x["rerank_score"])
+        
     end_time = time.time()
     return JSONResponse(
             {
                 "results": hits_list, 
-                "info": "version 2.0",
+                "info": "version 3.0",
                 "cost_time": f"{end_time - start_time:.4f}s"
             }, status_code=200)
 
@@ -300,9 +160,9 @@ def select_train_samples_by_coarse_type(coarse_types: list[str]) -> list[dict]:
         for cs_type in item['coarse_type_set'].split(','):
             if cs_type in coarse_types:
                 item_coarse_count += 1
-        item['coarse_count'] = item_coarse_count
+        item['hit_coarse_count'] = item_coarse_count
 
-    filtered_results.sort(key= lambda item: -item['coarse_count'])
+    filtered_results.sort(key= lambda item: -item['hit_coarse_count'])
 
     rt_list = []
     for datum in filtered_results:
@@ -316,14 +176,3 @@ def select_train_samples_by_coarse_type(coarse_types: list[str]) -> list[dict]:
         if modified_flag:
             rt_list.append(datum)
     return JSONResponse({"data": rt_list})
-
-# ======================
-# 5️⃣ API 接口
-# ======================
-@app.post("/select_samples")
-async def api_select_samples(request: Request):
-    body = await request.json()
-    test_coarse_types = body.get("coarse_type", None)
-    if test_coarse_types is None:
-        return JSONResponse({"error": "no test sample coarse_type"})
-    return select_train_samples_by_coarse_type(test_coarse_types)
